0cb88b25ae6af3819435beb4bb30b4b7afad85c4
Tiavina <80424305+Tiavina22@users.noreply.github.com>
2024-08-14 01:41:27 +0300

    Update ContactCard.tsx

--- app/components/ContactCard.tsx
+++ app/components/ContactCard.tsx
@@ -2,6 +2,7 @@ import { Card } from "@/components/ui/card";
 import { cn } from "@/lib/utils";
 import { ArrowUpRight, MessageCircle } from "lucide-react";
 import Link from "next/link";
+import Image from "next/image"
 
 export const ContactCard = (props: {
     image: string;
@@ -16,10 +17,10 @@ export const ContactCard = (props: {
         <Link href={props.url} className={cn("w-full", props.className) }>
         <Card className="p-3 bg-accent/10 hover:bg-accent/30 transition-colors group flex items-center gap-4">
             <div className="relative w-10 h-10">
-                <img src={props.image} alt={props.name} className="w-10 h-10 rounded-full object-contain" />
+                <Image src={props.image} alt={props.name} className="w-10 h-10 rounded-full object-contain" />
                 
                 {props.children}
-                <img src={props.mediumImage} alt={props.name} className="w-4 h-4 absolute -bottom-1 -right-1 rounded-full object-contain" />
+                <Image src={props.mediumImage} alt={props.name} className="w-4 h-4 absolute -bottom-1 -right-1 rounded-full object-contain" />
             </div>
             <div className="flex-1">
                 <div className="flex items-center gap-2">
