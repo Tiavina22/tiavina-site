ca9b17ad76af961f1947e624ea2cc5190fd5a9f5
Tiavina <80424305+Tiavina22@users.noreply.github.com>
2024-08-14 12:46:12 +0300

    Update Status.tsx

--- app/pages/Status.tsx
+++ app/pages/Status.tsx
@@ -80,7 +80,7 @@ const SIDE_PROJECTS: SideprojectProps[] = [
     {
         imageSrc: "/images/tiny.png",
         title: "Tiny",
-        description: "Tiny is a command-line CLI note manager developed in Dart. It enables users to add, list, delete, update, and search notes stored locally in a JSON file. Each note is identified by a unique ID, providing basic yet effective management of textual information. ",
+        description: "Tiny is a command-line CLI note manager developed in Dart. It enables users to add, list, delete, update, and search notes stored locally in a JSON file.",
         url: "https://github.com/Tiavina22/tiny",
         previewUrl: "https://github.com/Tiavina22/tiny",
         githubUrl: "https://github.com/Tiavina22/tiny"
